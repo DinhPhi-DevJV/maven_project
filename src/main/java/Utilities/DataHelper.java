a953c796aa63fa89a1665d8725e4fe7794eb565b
Nguyễn Đình Phi <phinguyen0806@gmail.com>
2023-04-26 10:36:57 +0700

    new commit

---
+++ src/main/java/Utilities/DataHelper.java
@@ -0,0 +1,34 @@
+package Utilities;
+
+import java.util.Locale;
+
+import com.github.javafaker.Faker;
+
+public class DataHelper {
+	//US
+
+	private Faker faker = new Faker();
+
+	public static DataHelper getDataHelper() {
+		return new DataHelper();
+	}
+	public String getFirstName() {
+		return faker.name().firstName();
+	}
+	public String getLastName() {
+		return faker.name().lastName();
+	}
+	public String getEamilAddress() {
+		return faker.internet().emailAddress();
+	}
+	public String getPhoneNumber() {
+		return faker.phoneNumber().cellPhone();
+	}
+	public String getCityName() {
+		return faker.address().cityName();
+	}
+	public String getPassword() {
+		return faker.internet().password(8,12,true,true);
+	}
+	
+}
